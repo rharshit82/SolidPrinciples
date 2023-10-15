3a9f8e344d21d1395cae825c5d091a6659d489b8
Harshit Raj <35199148+rharshit82@users.noreply.github.com>
2023-10-15 23:36:41 +0530

    fix: typing fixes

--- components/Landing/components/CodeSamples.tsx
+++ components/Landing/components/CodeSamples.tsx
@@ -1,10 +1,11 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
-const CodeSamples: React.FC<{ code: string }> = ({ code }) => {
-    return (
-      <SyntaxHighlighter style={docco}>
-        {code}
-      </SyntaxHighlighter>
-    );
-  };
+const CodeSamples = () => {
+  const codeString = "(num) => num + 1";
+  return (
+    <SyntaxHighlighter language='javascript' style={docco}>
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
 export default CodeSamples;
