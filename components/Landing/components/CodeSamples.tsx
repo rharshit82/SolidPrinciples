23170ff6bceec6f847f8ba1cd5a2182b8a374e23
Harshit Raj <35199148+rharshit82@users.noreply.github.com>
2023-10-15 23:34:15 +0530

    added types

--- components/Landing/components/CodeSamples.tsx
+++ components/Landing/components/CodeSamples.tsx
@@ -1,11 +1,10 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
-const CodeSamples = () => {
-  const codeString = "(num) => num + 1";
-  return (
-    <SyntaxHighlighter language='javascript' style={docco}>
-      {codeString}
-    </SyntaxHighlighter>
-  );
-};
+const CodeSamples: React.FC<{ code: string }> = ({ code }) => {
+    return (
+      <SyntaxHighlighter style={docco}>
+        {code}
+      </SyntaxHighlighter>
+    );
+  };
 export default CodeSamples;
